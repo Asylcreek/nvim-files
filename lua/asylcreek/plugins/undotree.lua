5951ecf547d7d26495d29af4fb98ccfa8fa4cbbd
Omokugbo Joseph Boro <omokugbobr@gmail.com>
2023-11-08 22:50:33 +0100

    feat: Add which-key.lua

--- lua/asylcreek/plugins/undotree.lua
+++ lua/asylcreek/plugins/undotree.lua
@@ -1,5 +1,5 @@
 return { 'mbbill/undotree', 
 	config = function()
-		vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+		vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Toggle undotree" })
 	end 
 }
