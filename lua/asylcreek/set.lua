1a57fe5f3184881ba7c60d346e7a1b5fed88a4d4
Omokugbo Joseph Boro <omokugbobr@gmail.com>
2023-11-08 21:41:15 +0100

    feat: Add set.lua

---
+++ lua/asylcreek/set.lua
@@ -0,0 +1,37 @@
+vim.opt.relativenumber = true
+vim.opt.number = true
+
+vim.opt.spell = true
+vim.opt.wrap = false
+
+vim.opt.showtabline = 2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+vim.opt.smartcase = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv "HOME" .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.signcolumn = "yes"
+vim.opt.scrolloff = 8
+vim.opt.colorcolumn = "80"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.cmdheight = 1
+
+vim.opt.clipboard = ""
+
+vim.g.mapleader = " "
