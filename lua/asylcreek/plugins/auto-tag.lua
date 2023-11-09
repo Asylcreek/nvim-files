2624d4b1eae9570308247592cc8aca0e9d1016cf
Omokugbo Joseph Boro <omokugbobr@gmail.com>
2023-11-09 11:54:05 +0100

    feat: Add auto-tag.lua

---
+++ lua/asylcreek/plugins/auto-tag.lua
@@ -0,0 +1,7 @@
+local filetypes = { 'typescript', 'javascript', 'typescriptreact', 'javascriptreact', 'html', 'xml', 'svelte' }
+
+return {
+  'windwp/nvim-ts-autotag',
+  opts = {},
+  ft = filetypes
+}
