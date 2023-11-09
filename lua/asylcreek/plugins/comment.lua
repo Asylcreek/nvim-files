c28f3a34650e8be05731f590c2357023888c30ef
Omokugbo Joseph Boro <omokugbobr@gmail.com>
2023-11-09 01:00:55 +0100

    feat: Add comment.lua

---
+++ lua/asylcreek/plugins/comment.lua
@@ -0,0 +1,19 @@
+return {
+  'numToStr/Comment.nvim',
+  event = "BufEnter",
+  config = function()
+    local comment = require "Comment"
+
+    comment.setup()
+
+    vim.keymap.set("n", "<leader>/",
+      function() require("Comment.api").toggle.linewise.count(vim.v.count > 0 and vim.v.count or 1) end,
+      { desc = "Toggle linewise comment" })
+
+    vim.keymap.set("v", "<leader>/",
+      "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>",
+      { desc = "Toggle comment for selection" })
+
+    return vim
+  end
+}
