d7184772077a997cfd055c34187c86e046d60391
Omokugbo Joseph Boro <omokugbobr@gmail.com>
2023-11-09 12:06:34 +0100

    feat: Add rainbow.lua

--- lua/asylcreek/plugins/treesitter.lua
+++ lua/asylcreek/plugins/treesitter.lua
@@ -1,35 +1,48 @@
 return {
-	'nvim-treesitter/nvim-treesitter',
-	build = ":TSUpdate",
-	config = function ()
-		require "nvim-treesitter.configs".setup {
-			ensure_installed = {
-				"javascript",
-				"typescript",
-				"css",
-				"lua",
-				"vim",
-				"vimdoc",
-				"query",
-				"html",
-				"dockerfile",
-				"gitignore",
-				"json",
-				"markdown",
-				"scss",
-				"sql",
-				"tsx",
-			},
+  'nvim-treesitter/nvim-treesitter',
+  build = ":TSUpdate",
+  config = function()
+    require "nvim-treesitter.configs".setup {
+      ensure_installed = {
+        "javascript",
+        "typescript",
+        "css",
+        "lua",
+        "vim",
+        "vimdoc",
+        "query",
+        "html",
+        "dockerfile",
+        "gitignore",
+        "json",
+        "markdown",
+        "scss",
+        "sql",
+        "tsx",
+      },
 
-			sync_install = false,
+      autotag = {
+        enable = true,
+        enable_rename = true,
+        enable_close = true,
+        enable_close_on_slash = true,
+      },
 
-			auto_install = true,
+      sync_install = false,
 
-			highlight = {
-				enable = true
-			},
+      auto_install = true,
 
-			indent = { enable = true }
-		}
-	end
+      highlight = {
+        enable = true
+      },
+
+      indent = { enable = true },
+
+      rainbow = {
+        enable = true,
+        extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+        max_file_lines = nil, -- Do not enable for files with more than n lines, int
+      },
+    }
+  end
 }
