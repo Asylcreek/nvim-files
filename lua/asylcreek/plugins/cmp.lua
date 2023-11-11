c5e85ed85635510e14e09647e656633775c04734
Omokugbo Joseph Boro <omokugbobr@gmail.com>
2023-11-11 09:40:13 +0100

    feat(cmp): Add signature-help source

--- lua/asylcreek/plugins/cmp.lua
+++ lua/asylcreek/plugins/cmp.lua
@@ -1,17 +1,19 @@
 return {
-  {
-    "L3MON4D3/LuaSnip",
-    build = vim.fn.has "win32" == 0 and "echo 'NOTE: jsregexp is optional, so not a big deal if it fails to build\n'; make install_jsregexp" or nil,
-    dependencies = { "rafamadriz/friendly-snippets" },
+	{
+		"L3MON4D3/LuaSnip",
+		build = vim.fn.has("win32") == 0
+				and "echo 'NOTE: jsregexp is optional, so not a big deal if it fails to build\n'; make install_jsregexp"
+			or nil,
+		dependencies = { "rafamadriz/friendly-snippets" },
 		config = function()
-			local luasnip = require "luasnip"
+			local luasnip = require("luasnip")
 
 			luasnip.filetype_extend("javascript", { "javascriptreact" })
 			luasnip.filetype_extend("typescript", { "typescriptreact" })
 
 			require("luasnip.loaders.from_vscode").load()
-		end
-  },
+		end,
+	},
 	{
 		"hrsh7th/nvim-cmp",
 		dependencies = {
@@ -19,13 +21,16 @@ return {
 			"hrsh7th/cmp-buffer",
 			"hrsh7th/cmp-path",
 			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-nvim-lsp-signature-help",
 		},
 		event = "InsertEnter",
 		opts = function()
-			local cmp = require "cmp"
+			local cmp = require("cmp")
 			local snip_status_ok, luasnip = pcall(require, "luasnip")
 
-			if not snip_status_ok then return end
+			if not snip_status_ok then
+				return
+			end
 
 			local border_opts = {
 				border = "rounded",
@@ -34,17 +39,20 @@ return {
 
 			local function has_words_before()
 				local line, col = (unpack or table.unpack)(vim.api.nvim_win_get_cursor(0))
-				return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+				return col ~= 0
+					and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 			end
 
 			return {
 				enabled = true,
 				preselect = cmp.PreselectMode.None,
 				formatting = {
-					fields = {"abbr", "menu", "kind"}
+					fields = { "abbr", "menu", "kind" },
 				},
 				snippet = {
-					expand = function(args) luasnip.lsp_expand(args.body) end,
+					expand = function(args)
+						luasnip.lsp_expand(args.body)
+					end,
 				},
 				duplicates = {
 					nvim_lsp = 1,
@@ -62,18 +70,18 @@ return {
 					documentation = cmp.config.window.bordered(border_opts),
 				},
 				mapping = {
-					["<Up>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
-					["<Down>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
-					["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
-					["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
-					["<C-k>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
-					["<C-j>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
+					["<Up>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+					["<Down>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+					["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+					["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+					["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+					["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
 					["<C-u>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
 					["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
 					["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
 					["<C-y>"] = cmp.config.disable,
-					["<C-e>"] = cmp.mapping { i = cmp.mapping.abort(), c = cmp.mapping.close() },
-					["<CR>"] = cmp.mapping.confirm { select = false },
+					["<C-e>"] = cmp.mapping({ i = cmp.mapping.abort(), c = cmp.mapping.close() }),
+					["<CR>"] = cmp.mapping.confirm({ select = false }),
 					["<Tab>"] = cmp.mapping(function(fallback)
 						if cmp.visible() then
 							cmp.select_next_item()
@@ -95,14 +103,14 @@ return {
 						end
 					end, { "i", "s" }),
 				},
-				sources = cmp.config.sources {
+				sources = cmp.config.sources({
 					{ name = "nvim_lsp", priority = 1000 },
 					{ name = "luasnip", priority = 750 },
 					{ name = "buffer", priority = 500 },
 					{ name = "path", priority = 250 },
-				}
+					{ name = "nvim_lsp_signature_help", priority = 200 },
+				}),
 			}
-		end
-	}
+		end,
+	},
 }
-
