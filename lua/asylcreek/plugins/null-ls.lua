3bb5b6db937ec454d54ac935e1e3e401fff11ea4
Omokugbo Joseph Boro <omokugbobr@gmail.com>
2023-11-12 22:16:20 +0100

    refactor: Properly setup diagnostics config

--- lua/asylcreek/plugins/null-ls.lua
+++ lua/asylcreek/plugins/null-ls.lua
@@ -1,25 +1,27 @@
 return {
-  "jose-elias-alvarez/null-ls.nvim",
-  event = "BufEnter",
-  config = function()
-    local null_ls = require("null-ls")
+	"jose-elias-alvarez/null-ls.nvim",
+	event = "BufEnter",
+	config = function()
+		local null_ls = require("null-ls")
 
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.diagnostics.eslint,
-        null_ls.builtins.completion.spell,
-        null_ls.builtins.diagnostics.cspell.with {
-          diagnostics_postprocess = function(diagnostic) diagnostic.severity = vim.diagnostic.severity.WARN end,
-        },
-      },
-      diagnostic_config = {
-        virtual_text = true,
-        underline = true,
-        update_in_insert = true,
-      },
-      update_in_insert = true
-    })
-  end
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.diagnostics.eslint,
+				null_ls.builtins.completion.spell,
+				null_ls.builtins.diagnostics.cspell.with({
+					diagnostics_postprocess = function(diagnostic)
+						diagnostic.severity = vim.diagnostic.severity.WARN
+					end,
+				}),
+			},
+		})
+
+		vim.diagnostic.config({
+			virtual_text = true,
+			underline = true,
+			update_in_insert = true,
+		})
+	end,
 }
