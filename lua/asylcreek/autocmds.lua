d0225bb82dca326275fbc152916e04ad2670f778
Omokugbo Joseph Boro <omokugbobr@gmail.com>
2023-11-16 10:00:38 +0100

    feat: Add autocmd to go to last line when file opens

--- lua/asylcreek/autocmds.lua
+++ lua/asylcreek/autocmds.lua
@@ -1,8 +1,7 @@
-local resession = require "resession"
+local resession = require("resession")
 
 vim.api.nvim_create_autocmd("VimEnter", {
   callback = function()
-
     -- Only load the session if nvim was started with no args
     if vim.fn.argc(-1) == 0 then
       -- Save these to a different directory, so our manual sessions don't get polluted
@@ -13,5 +12,27 @@ vim.api.nvim_create_autocmd("VimEnter", {
 
 -- Save session before exiting
 vim.api.nvim_create_autocmd("VimLeavePre", {
-  callback = function() resession.save(vim.fn.getcwd(), { dir = "dirsession", notify = false }) end,
+  callback = function()
+    resession.save(vim.fn.getcwd(), { dir = "dirsession", notify = false })
+  end,
+})
+
+vim.api.nvim_create_autocmd("BufRead", {
+  callback = function(opts)
+    vim.api.nvim_create_autocmd("BufWinEnter", {
+      once = true,
+      buffer = opts.buf,
+      callback = function()
+        local ft = vim.bo[opts.buf].filetype
+        local last_known_line = vim.api.nvim_buf_get_mark(opts.buf, '"')[1]
+        if
+            not (ft:match("commit") and ft:match("rebase"))
+            and last_known_line > 1
+            and last_known_line <= vim.api.nvim_buf_line_count(opts.buf)
+        then
+          vim.api.nvim_feedkeys([[g`"]], "nx", false)
+        end
+      end,
+    })
+  end,
 })
