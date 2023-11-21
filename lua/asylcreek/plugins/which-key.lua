f100ee9cbc8d3166c4ebdd0aa723c388b912896c
Omokugbo Joseph Boro <omokugbobr@gmail.com>
2023-11-21 21:44:45 +0100

    feat: Add octo.nvim

--- lua/asylcreek/plugins/which-key.lua
+++ lua/asylcreek/plugins/which-key.lua
@@ -5,37 +5,53 @@ return {
     vim.o.timeout = true
     vim.o.timeoutlen = 300
   end,
-  config = function ()
-    local wk = require "which-key"
+  config = function()
+    local wk = require("which-key")
 
     wk.setup()
 
-    wk.register ({
-      f = { name = "Find" }
-    }, {mode = "n", prefix = "<leader>" })
+    wk.register({
+      f = { name = "Find" },
+    }, { mode = "n", prefix = "<leader>" })
 
-    wk.register ({
-      b = { name = "Buffers" }
-    }, {mode = "n", prefix = "<leader>" })
+    wk.register({
+      b = { name = "Buffers" },
+    }, { mode = "n", prefix = "<leader>" })
 
-    wk.register ({
-      g = { name = "Git" }
-    }, {mode = "n", prefix = "<leader>" })
+    wk.register({
+      g = { name = "Git" },
+    }, { mode = "n", prefix = "<leader>" })
 
-    wk.register ({
-      h = { name = "Harpoon" }
-    }, {mode = "n", prefix = "<leader>" })
+    wk.register({
+      h = { name = "Harpoon" },
+    }, { mode = "n", prefix = "<leader>" })
 
-    wk.register ({
-      r = { name = "Resession" }
-    }, {mode = "n", prefix = "<leader>" })
+    wk.register({
+      r = { name = "Resession" },
+    }, { mode = "n", prefix = "<leader>" })
 
-    wk.register ({
-      l = { name = "LSP" }
-    }, {mode = "n", prefix = "<leader>" })
+    wk.register({
+      l = { name = "LSP" },
+    }, { mode = "n", prefix = "<leader>" })
 
-    wk.register ({
-      x = { name = "Trouble" }
-    }, {mode = "n", prefix = "<leader>" })
-  end
+    wk.register({
+      x = { name = "Trouble" },
+    }, { mode = "n", prefix = "<leader>" })
+
+    wk.register({
+      d = { name = "Diagnostics" },
+    }, { mode = "n", prefix = "<leader>" })
+
+    wk.register({
+      o = { name = "Octo" },
+    }, { mode = "n", prefix = "<leader>" })
+
+    wk.register({
+      r = { name = "Reviews" },
+    }, { mode = "n", prefix = "<leader>o" })
+
+    wk.register({
+      s = { name = "Start/Submit" },
+    }, { mode = "n", prefix = "<leader>or" })
+  end,
 }
