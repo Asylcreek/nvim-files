61ccab0048cd1ff35b2bba7e19d4c88d6a2c040f
Omokugbo Joseph Boro <omokugbobr@gmail.com>
2023-12-07 15:53:53 +0100

    feat: Get harpoon from harpoon2 branch instead

--- lua/asylcreek/plugins/harpoon.lua
+++ lua/asylcreek/plugins/harpoon.lua
@@ -1,30 +1,35 @@
 return {
   "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  requires = { "nvim-lua/plenary.nvim" },
   event = "VeryLazy",
   config = function()
-    local mark = require("harpoon.mark")
-    local ui = require("harpoon.ui")
+    local harpoon = require("harpoon")
 
-    vim.keymap.set("n", "<leader>ha", mark.add_file, { desc = "Add file to harpoon" })
-    vim.keymap.set("n", "<leader>he", ui.toggle_quick_menu, { desc = "Show harpoon ui" })
+    vim.keymap.set("n", "<leader>ha", function()
+      harpoon:list():append()
+    end, { desc = "Add file to harpoon" })
+    vim.keymap.set("n", "<leader>he", function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end, { desc = "Show harpoon ui" })
 
     vim.keymap.set("n", "<leader>hh", function()
-      ui.nav_file(1)
+      harpoon:list():select(1)
     end, { desc = "Navigate to first harpoon file" })
     vim.keymap.set("n", "<leader>hj", function()
-      ui.nav_file(2)
+      harpoon:list():select(2)
     end, { desc = "Navigate to second harpoon file" })
     vim.keymap.set("n", "<leader>hk", function()
-      ui.nav_file(3)
+      harpoon:list():select(3)
     end, { desc = "Navigate to third harpoon file" })
     vim.keymap.set("n", "<leader>hl", function()
-      ui.nav_file(4)
+      harpoon:list():select(4)
     end, { desc = "Navigate to fourth harpoon file" })
     vim.keymap.set("n", "<leader>hn", function()
-      ui.nav_next()
+      harpoon:list():next()
     end, { desc = "Navigate to next harpoon file" })
     vim.keymap.set("n", "<leader>hp", function()
-      ui.nav_prev()
+      harpoon:list():previous()
     end, { desc = "Navigate to previous harpoon file" })
 
     return vim
