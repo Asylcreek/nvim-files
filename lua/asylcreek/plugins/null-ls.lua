600e41c38dd866ce29a5496e20b459bc1bd8153d
Omokugbo Joseph Boro <omokugbobr@gmail.com>
2023-12-07 12:55:51 +0100

    feat: Improve null-ls config

--- lua/asylcreek/plugins/null-ls.lua
+++ lua/asylcreek/plugins/null-ls.lua
@@ -1,19 +1,43 @@
 return {
   "jose-elias-alvarez/null-ls.nvim",
+  dependencies = { "davidmh/cspell.nvim" },
   event = "BufEnter",
   config = function()
     local null_ls = require("null-ls")
+    local cspell = require("cspell")
+
+    local cspellConfig = {
+      find_json = function()
+        return vim.fn.expand("$HOME/.config/cspell/cspell.json")
+      end,
+    }
 
     null_ls.setup({
       sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.diagnostics.eslint,
+        cspell.code_actions.with({
+          config = cspellConfig,
+        }),
+        null_ls.builtins.code_actions.gomodifytags,
+
         null_ls.builtins.completion.spell,
-        null_ls.builtins.diagnostics.cspell.with({
+
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettier.with({
+          extra_filetypes = { "svelte" },
+        }),
+        null_ls.builtins.formatting.gofmt,
+        null_ls.builtins.formatting.goimports,
+        null_ls.builtins.formatting.goimports_reviser,
+        null_ls.builtins.formatting.golines,
+        null_ls.builtins.formatting.prismaFmt,
+
+        null_ls.builtins.diagnostics.eslint_d,
+        null_ls.builtins.diagnostics.puglint,
+        cspell.diagnostics.with({
           diagnostics_postprocess = function(diagnostic)
-            diagnostic.severity = vim.diagnostic.severity.WARN
+            diagnostic.severity = vim.diagnostic.severity.INFO
           end,
+          config = cspellConfig,
         }),
       },
     })
