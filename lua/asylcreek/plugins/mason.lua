de11534f34c6ebbe4847167f53c2df1262618a0b
Omokugbo Joseph Boro <omokugbobr@gmail.com>
2024-04-18 16:35:36 +0100

    chore(mason): Remove cssmodules modification

--- lua/asylcreek/plugins/mason.lua
+++ lua/asylcreek/plugins/mason.lua
@@ -1,70 +1,57 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    event = "BufEnter",
-    config = function()
-      require("mason").setup({})
-    end,
-  },
+	{
+		"williamboman/mason.nvim",
+		event = "BufEnter",
+		config = function()
+			require("mason").setup({})
+		end,
+	},
 
-  {
-    "williamboman/mason-lspconfig.nvim",
-    event = "BufEnter",
-    config = function()
-      local lsp_zero = require("lsp-zero")
+	{
+		"williamboman/mason-lspconfig.nvim",
+		event = "BufEnter",
+		config = function()
+			local lsp_zero = require("lsp-zero")
 
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "tsserver",
-          "cssls",
-          "html",
-          "dockerls",
-          "jsonls",
-          "docker_compose_language_service",
-          "eslint",
-          "emmet_ls",
-        },
-        handlers = {
-          lsp_zero.default_setup,
-          ["lua_ls"] = function()
-            local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({
-              settings = {
-                Lua = {
-                  diagnostics = {
-                    globals = { "vim" },
-                  },
-                },
-              },
-            })
-          end,
-          cssmodules_ls = function()
-            local lspconfig = require("lspconfig")
-            lspconfig.cssmodules_ls.setup({
-              filetypes = {
-                "javascript",
-                "javascriptreact",
-                "typescript",
-                "typescriptreact",
-                "scss",
-                "css",
-              },
-            })
-          end,
-        },
-      })
-    end,
-  },
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"tsserver",
+					"cssls",
+					"html",
+					"dockerls",
+					"jsonls",
+					"docker_compose_language_service",
+					"eslint",
+					"emmet_ls",
+				},
+				handlers = {
+					lsp_zero.default_setup,
+					["lua_ls"] = function()
+						local lspconfig = require("lspconfig")
+						lspconfig.lua_ls.setup({
+							settings = {
+								Lua = {
+									diagnostics = {
+										globals = { "vim" },
+									},
+								},
+							},
+						})
+					end,
+				},
+			})
+		end,
+	},
 
-  {
-    "jay-babu/mason-null-ls.nvim",
-    event = "BufEnter",
-    dependencies = {
-      "jose-elias-alvarez/null-ls.nvim",
-    },
-    config = function()
-      require("asylcreek.plugins.null-ls")
-    end,
-  },
+	{
+		"jay-babu/mason-null-ls.nvim",
+		event = "BufEnter",
+		dependencies = {
+			"jose-elias-alvarez/null-ls.nvim",
+		},
+		config = function()
+			require("asylcreek.plugins.null-ls")
+		end,
+	},
 }
