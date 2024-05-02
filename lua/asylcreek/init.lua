8ff27f6e3637ef0673bd090d98134f91633c9018
Omokugbo Joseph Boro <omokugbobr@gmail.com>
2024-05-02 11:39:48 +0100

    feat(colors): Add ayu

--- lua/asylcreek/init.lua
+++ lua/asylcreek/init.lua
@@ -4,7 +4,7 @@ require("asylcreek.lazy")
 require("asylcreek.autocmds")
 
 vim.opt.background = "dark"
-vim.cmd("colorscheme duckbones")
+vim.cmd("colorscheme ayu-mirage")
 
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
--- lua/asylcreek/plugins/colors.lua
+++ lua/asylcreek/plugins/colors.lua
@@ -1,59 +1,71 @@
 return {
-  {
-    "kvrohit/mellow.nvim",
-    lazy = "VeryLazy",
-  },
-  {
-    "olimorris/onedarkpro.nvim",
-    lazy = "false",
-    opts = {
-      options = {
-        transparency = true,
-        terminal_colors = false,
-      },
-    },
-  },
-  {
-    "rebelot/kanagawa.nvim",
-    lazy = "VeryLazy",
-    config = function()
-      local kanagawa = require("kanagawa")
+	{
+		"kvrohit/mellow.nvim",
+		lazy = "VeryLazy",
+	},
+	{
+		"olimorris/onedarkpro.nvim",
+		lazy = "false",
+		opts = {
+			options = {
+				transparency = true,
+				terminal_colors = false,
+			},
+		},
+	},
+	{
+		"rebelot/kanagawa.nvim",
+		lazy = "VeryLazy",
+		config = function()
+			local kanagawa = require("kanagawa")
 
-      kanagawa.setup({
-        transparent = false,
-        colors = {
-          theme = {
-            all = {
-              ui = {
-                bg_gutter = "none",
-              },
-            },
-          },
-        },
-      })
-    end,
-  },
-  {
-    "EdenEast/nightfox.nvim",
-    lazy = "VeryLazy",
-    opts = {
-      options = {
-        transparent = true,
-      },
-      groups = {
-        all = {
-          DiagnosticVirtualTextInfo = { bg = "none" },
-          DiagnosticVirtualTextError = { bg = "none" },
-          DiagnosticVirtualTextWarn = { bg = "none" },
-          DiagnosticVirtualTextHint = { bg = "none" },
-          DiagnosticVirtualTextOk = { bg = "none" },
-        },
-      },
-    },
-  },
-  {
-    "mcchrish/zenbones.nvim",
-    dependencies = "rktjmp/lush.nvim",
-    lazy = "VeryLazy",
-  },
+			kanagawa.setup({
+				transparent = false,
+				colors = {
+					theme = {
+						all = {
+							ui = {
+								bg_gutter = "none",
+							},
+						},
+					},
+				},
+			})
+		end,
+	},
+	{
+		"EdenEast/nightfox.nvim",
+		lazy = "VeryLazy",
+		opts = {
+			options = {
+				transparent = true,
+			},
+			groups = {
+				all = {
+					DiagnosticVirtualTextInfo = { bg = "none" },
+					DiagnosticVirtualTextError = { bg = "none" },
+					DiagnosticVirtualTextWarn = { bg = "none" },
+					DiagnosticVirtualTextHint = { bg = "none" },
+					DiagnosticVirtualTextOk = { bg = "none" },
+				},
+			},
+		},
+	},
+	{
+		"mcchrish/zenbones.nvim",
+		dependencies = "rktjmp/lush.nvim",
+		lazy = "VeryLazy",
+	},
+	{
+		"Shatur/neovim-ayu",
+		lazy = "VeryLazy",
+		config = function()
+			require("ayu").setup({
+				overrides = {
+					SignColumn = { bg = "None" },
+					FoldColumn = { bg = "None" },
+				},
+			})
+		end,
+	},
 }
