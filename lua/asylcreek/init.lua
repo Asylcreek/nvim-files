7c528e035f00f78b932f8a42a2561e9507e3813a
Omokugbo Joseph Boro <omokugbobr@gmail.com>
2024-06-01 19:44:47 +0100

    feat(init): Change winsep border color

--- lua/asylcreek/init.lua
+++ lua/asylcreek/init.lua
@@ -8,3 +8,6 @@ vim.cmd("colorscheme ayu-mirage")
 
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+-- set border color
+vim.cmd.hi("WinSeparator guifg=orange")
