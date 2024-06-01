0182d3567718a99409ac7a86227e4e056750461f
Omokugbo Joseph Boro <omokugbobr@gmail.com>
2024-06-01 19:43:48 +0100

    chore: Update packages

--- lua/asylcreek/plugins/auto-tag.lua
+++ lua/asylcreek/plugins/auto-tag.lua
@@ -1,10 +1,13 @@
 return {
 	"windwp/nvim-ts-autotag",
-	opts = {
-		enable = true,
-		enable_rename = true,
-		enable_close = true,
-		enable_close_on_slash = true,
-	},
-	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		require("nvim-ts-autotag").setup({
+			opts = {
+				enable = true,
+				enable_rename = true,
+				enable_close = true,
+				enable_close_on_slash = true,
+			},
+		})
+	end,
 }
