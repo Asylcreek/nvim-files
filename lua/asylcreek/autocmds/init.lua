af0b1832c260037c8d10294a5b5234a05371ff11
Omokugbo Joseph Boro <omokugbobr@gmail.com>
2024-06-21 10:28:22 +0100

    feat: Update docker filetype

--- lua/asylcreek/autocmds/init.lua
+++ lua/asylcreek/autocmds/init.lua
@@ -1,24 +1,25 @@
 require("asylcreek.autocmds.resession")
 require("asylcreek.autocmds.minifiles")
 require("asylcreek.autocmds.terraform")
+require("asylcreek.autocmds.docker")
 
 -- Open file to last location before exiting
 vim.api.nvim_create_autocmd("BufRead", {
-  callback = function(opts)
-    vim.api.nvim_create_autocmd("BufWinEnter", {
-      once = true,
-      buffer = opts.buf,
-      callback = function()
-        local ft = vim.bo[opts.buf].filetype
-        local last_known_line = vim.api.nvim_buf_get_mark(opts.buf, '"')[1]
-        if
-            not (ft:match("commit") and ft:match("rebase"))
-            and last_known_line > 1
-            and last_known_line <= vim.api.nvim_buf_line_count(opts.buf)
-        then
-          vim.api.nvim_feedkeys([[g`"]], "nx", false)
-        end
-      end,
-    })
-  end,
+	callback = function(opts)
+		vim.api.nvim_create_autocmd("BufWinEnter", {
+			once = true,
+			buffer = opts.buf,
+			callback = function()
+				local ft = vim.bo[opts.buf].filetype
+				local last_known_line = vim.api.nvim_buf_get_mark(opts.buf, '"')[1]
+				if
+					not (ft:match("commit") and ft:match("rebase"))
+					and last_known_line > 1
+					and last_known_line <= vim.api.nvim_buf_line_count(opts.buf)
+				then
+					vim.api.nvim_feedkeys([[g`"]], "nx", false)
+				end
+			end,
+		})
+	end,
 })
