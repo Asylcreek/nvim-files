4f82271d185e51b4f0a2d27dac415076b8395af9
Omokugbo Joseph Boro <omokugbobr@gmail.com>
2024-12-23 09:42:33 +0100

    refactor: Add more options

--- lua/asylcreek/plugins/hardtime.lua
+++ lua/asylcreek/plugins/hardtime.lua
@@ -1,5 +1,16 @@
 return {
 	"m4xshen/hardtime.nvim",
 	dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-	opts = {},
+	opts = {
+		restricted_keys = {
+			["e"] = { "n", "x" },
+			["b"] = { "n", "x" },
+			["<BS>"] = { "", "i" },
+			["<Del>"] = { "", "i" },
+		},
+		disabled_keys = {
+			["<BS>"] = { "", "i" },
+			["<Del>"] = { "", "i" },
+		},
+	},
 }
